b59fc287089d568f57e1524efd43ac24c76b42b1
Ruslan Klymenko <ruslan.klymenko@unicornsystems.eu>
2017-08-09 11:21:29 +0300

    Kotlin integration

---
+++ cryptocurrencyWeb/src/main/java/com/outsourcebooster/cryptocurrency/web/config/security/filter/JwtAuthenticationTokenFilter.kt
@@ -0,0 +1,42 @@
+package com.outsourcebooster.cryptocurrency.web.config.security.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.outsourcebooster.cryptocurrency.web.config.security.util.JwtTokenUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+open class JwtAuthenticationTokenFilter: OncePerRequestFilter() {
+
+    @Autowired
+    private lateinit var userDetailsService: UserDetailsService
+    @Autowired
+    private lateinit var jwtTokenUtil: JwtTokenUtil
+    @Value("\${jwt.header}")
+    private lateinit var tokenHeader: String
+
+    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, chain: FilterChain) {
+        val authToken = request.getHeader(this.tokenHeader)
+        if(authToken != null) {
+            val username = jwtTokenUtil.getUsernameFromToken(authToken)
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                val userDetails = this.userDetailsService.loadUserByUsername(username)
+                if (jwtTokenUtil.validateToken(authToken, userDetails)) {
+                    val authentication = UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities())
+                    SecurityContextHolder.getContext().setAuthentication(authentication)
+                }
+            }
+        }
+        chain.doFilter(request, response)
+    }
+}
